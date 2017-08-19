a8d4b354fbbf8c83595641f7822623e31988102a
Tanya Gupta <tanya.harsh@gmail.com>
2017-08-19 09:37:22 +0530

    first attempt

---
+++ app/src/main/java/com/example/tanya/crystalize/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tanya.crystalize;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_page);
+    }
+    public void onSplashPageClick(View view) {
+        Intent intent = new Intent(this, questions.class);
+        startActivity(intent);
+
+
+    }
+}
